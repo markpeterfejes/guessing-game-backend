415bb7258bf4f91ddb6da5f026287397be7a3a5e
MARKPETERFEJES\fejes <mfejes@korzo.co>
2017-09-01 10:48:52 +0200

    Ignore the dist folder

--- dist/src/models/logEntry.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const mongoose_1 = require("mongoose");
-const LogEntrySchema = new mongoose_1.Schema({
-    guess: { type: Number, required: true },
-    solution: { type: Number, required: true },
-});
-exports.LogEntry = mongoose_1.model("LogEntry", LogEntrySchema);
-//# sourceMappingURL=logEntry.js.map
